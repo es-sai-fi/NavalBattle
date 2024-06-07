edbd286bdb64ec11ff7c963562ff10bab8965a9e
Jamh4949 <163215234+Jamh4949@users.noreply.github.com>
2024-06-07 14:59:09 -0500

    Update LoseStage.java

--- src/main/java/org/example/navalbattle/view/LoseStage.java
+++ src/main/java/org/example/navalbattle/view/LoseStage.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jose Martínez - Jhorman Gómez - Esteban Gómez
+ * @version final
+ */
 package org.example.navalbattle.view;
 
 import javafx.fxml.FXMLLoader;
@@ -6,11 +10,19 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 
+/**
+ * Represents the lose stage (window) for the naval battle game.
+ * This class handles the loading of the FXML layout for the lose view.
+ */
 public class LoseStage extends Stage {
 
+    /**
+     * Constructs a LoseStage and initializes the lose view by loading the FXML layout.
+     *
+     * @throws IOException if there is an error loading the FXML file
+     */
     public LoseStage() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/navalbattle/lose-view.fxml"));
         Parent root;
@@ -29,17 +41,30 @@ public class LoseStage extends Stage {
         show();
     }
 
+    /**
+     * Returns the singleton instance of LoseStage.
+     *
+     * @return the singleton instance of LoseStage
+     * @throws IOException if there is an error creating the instance
+     */
     public static LoseStage getInstance() throws IOException {
-        return  LoseStage.LoseStageHolder.INSTANCE != null ?
+        return LoseStage.LoseStageHolder.INSTANCE != null ?
                 LoseStage.LoseStageHolder.INSTANCE :
                 (LoseStage.LoseStageHolder.INSTANCE = new LoseStage());
     }
 
+    /**
+     * Deletes the singleton instance of LoseStage.
+     * Closes the stage and sets the instance to null.
+     */
     public static void deleteInstance() {
-        LoseStageHolder.INSTANCE.close();
-        LoseStageHolder.INSTANCE = null;
+        LoseStage.LoseStageHolder.INSTANCE.close();
+        LoseStage.LoseStageHolder.INSTANCE = null;
     }
 
+    /**
+     * Holder class for the singleton instance of LoseStage.
+     */
     private static class LoseStageHolder {
         private static LoseStage INSTANCE;
     }
