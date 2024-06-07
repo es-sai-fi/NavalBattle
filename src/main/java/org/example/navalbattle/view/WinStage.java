772d0f93023253e17cb5a22f1a526d76263d2e73
Jamh4949 <163215234+Jamh4949@users.noreply.github.com>
2024-06-07 15:01:16 -0500

    Update WinStage.java

--- src/main/java/org/example/navalbattle/view/WinStage.java
+++ src/main/java/org/example/navalbattle/view/WinStage.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jose Martínez - Jhorman Gómez - Esteban Gómez
+ * @version final
+ */
 package org.example.navalbattle.view;
 
 import javafx.fxml.FXMLLoader;
@@ -8,9 +12,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * This class represents the stage displayed when the player wins the Naval Battle game.
+ */
 public class WinStage extends Stage {
 
-
+    /**
+     * Constructs a new WinStage.
+     * This constructor loads the FXML file and sets up the stage with the appropriate scene and properties.
+     *
+     * @throws IOException if the FXML file cannot be loaded.
+     */
     public WinStage() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/navalbattle/win-view.fxml"));
         Parent root;
@@ -30,17 +42,31 @@ public class WinStage extends Stage {
         show();
     }
 
+    /**
+     * Retrieves the single instance of WinStage.
+     * If the instance does not exist, it will be created.
+     *
+     * @return the single instance of WinStage.
+     * @throws IOException if the FXML file cannot be loaded during the instance creation.
+     */
     public static WinStage getInstance() throws IOException {
-        return  WinStage.WinStageHolder.INSTANCE != null ?
+        return WinStage.WinStageHolder.INSTANCE != null ?
                 WinStage.WinStageHolder.INSTANCE :
                 (WinStage.WinStageHolder.INSTANCE = new WinStage());
     }
 
+    /**
+     * Deletes the current instance of WinStage by closing the stage and setting the instance to null.
+     */
     public static void deleteInstance() {
         WinStageHolder.INSTANCE.close();
         WinStageHolder.INSTANCE = null;
     }
 
+    /**
+     * Holder class for the WinStage instance.
+     * This ensures that the instance is lazily initialized.
+     */
     private static class WinStageHolder {
         private static WinStage INSTANCE;
     }
