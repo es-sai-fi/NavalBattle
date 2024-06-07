14aa9759177b01170b7339b4791631b658c07e3f
Jamh4949 <163215234+Jamh4949@users.noreply.github.com>
2024-06-07 14:48:19 -0500

    Update GameController.java

--- src/main/java/org/example/navalbattle/controller/GameController.java
+++ src/main/java/org/example/navalbattle/controller/GameController.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jose Martínez - Jhorman Gómez - Esteban Gómez
+ * @version final
+ */
 package org.example.navalbattle.controller;
 
 import javafx.event.ActionEvent;
@@ -16,7 +20,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GameController{
+/**
+ * Controller class for handling the game's logic and interactions.
+ * This class is responsible for initializing the game boards, handling user actions,
+ * and managing the game state.
+ */
+public class GameController {
     @FXML
     private GridPane playerBoard;
     @FXML
@@ -25,11 +34,19 @@ public class GameController{
     private Button showEnemyBoardButton;
     @FXML
     private ImageView showEnemyBoardImage;
+
     private boolean continueGame = false;
     private List<ShipDrawingData> playerShipsDrawingData = new ArrayList<>();
     private List<ShipDrawing> enemyShipDrawings = new ArrayList<>();
     private NavalBattle navalBattle;
 
+    /**
+     * Initializes the game controller with the given naval battle instance.
+     * Sets up the enemy and player boards and serializes the game state.
+     *
+     * @param navalBattle the naval battle instance
+     * @throws IOException if an I/O error occurs during serialization
+     */
     public void initialize(NavalBattle navalBattle) throws IOException {
         this.navalBattle = navalBattle;
         createEnemyBoard();
@@ -38,8 +55,15 @@ public class GameController{
         NavalBattleSerialize.serialize(this.navalBattle);
     }
 
+    /**
+     * Continues the game by deserializing the saved game state and arranging the boards.
+     *
+     * @return true if the game state was successfully loaded, false otherwise
+     * @throws IOException if an I/O error occurs during deserialization
+     * @throws ClassNotFoundException if the class of a serialized object cannot be found
+     */
     public boolean continueGame() throws IOException, ClassNotFoundException {
-        try{
+        try {
             navalBattle = (NavalBattle) NavalBattleSerialize.deserialize("navalBattle.nvl");
             continueGame = true;
             navalBattle.arrangePlayerBoard(continueGame, playerBoard);
@@ -53,6 +77,9 @@ public class GameController{
         }
     }
 
+    /**
+     * Creates and sets up the enemy board with clickable cells that handle attack events.
+     */
     public void createEnemyBoard() {
         Cell[][] enemyBoardAux = new Cell[10][10];
         for (int row = 0; row < 10; row++) {
@@ -68,22 +95,37 @@ public class GameController{
         navalBattle.setEnemyBoardAux(enemyBoardAux);
     }
 
+    /**
+     * Handles the action of showing the enemy board when the corresponding button is clicked.
+     *
+     * @param actionEvent the action event triggered by clicking the button
+     */
     @FXML
-    void onShowEnemyBoardButtonClick(ActionEvent actionEvent){
-        for(ShipDrawing enemyShipDrawing : enemyShipDrawings){
+    void onShowEnemyBoardButtonClick(ActionEvent actionEvent) {
+        for (ShipDrawing enemyShipDrawing : enemyShipDrawings) {
             enemyShipDrawing.setVisible(true);
         }
         showEnemyBoardImage.setVisible(false);
         showEnemyBoardButton.setVisible(false);
     }
 
+    /**
+     * Sets the player ships drawing data.
+     *
+     * @param playerShipsDrawingData the list of player ship drawing data
+     */
     public void setPlayerShipsDrawingData(List<ShipDrawingData> playerShipsDrawingData) {
         this.playerShipsDrawingData = playerShipsDrawingData;
     }
 
+    /**
+     * Handles the winning scenario by deleting the save file, disabling the boards, and displaying the win stage.
+     *
+     * @throws IOException if an I/O error occurs during file deletion
+     */
     public void win() throws IOException {
         File serializedNavalBattle = new File("A:\\IntelliJProyectos\\NavalBattle\\navalBattle.nvl");
-        if(serializedNavalBattle.delete()){
+        if (serializedNavalBattle.delete()) {
             System.out.println("Save file deleted.");
         }
         playerBoard.setDisable(true);
@@ -91,9 +133,14 @@ public class GameController{
         WinStage.getInstance();
     }
 
+    /**
+     * Handles the losing scenario by deleting the save file, disabling the boards, and displaying the lose stage.
+     *
+     * @throws IOException if an I/O error occurs during file deletion
+     */
     public void lose() throws IOException {
         File serializedNavalBattle = new File("A:\\IntelliJProyectos\\NavalBattle\\navalBattle.nvl");
-        if(serializedNavalBattle.delete()){
+        if (serializedNavalBattle.delete()) {
             System.out.println("Save file deleted.");
         }
         playerBoard.setDisable(true);
