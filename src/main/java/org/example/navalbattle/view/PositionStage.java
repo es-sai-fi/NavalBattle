ebdf89a1bb79cca1649562e23ce92d74c2fd2350
Jamh4949 <163215234+Jamh4949@users.noreply.github.com>
2024-06-07 14:59:34 -0500

    Update PositionStage.java

--- src/main/java/org/example/navalbattle/view/PositionStage.java
+++ src/main/java/org/example/navalbattle/view/PositionStage.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jose Martínez - Jhorman Gómez - Esteban Gómez
+ * @version final
+ */
 package org.example.navalbattle.view;
 
 import javafx.fxml.FXMLLoader;
@@ -9,9 +13,18 @@ import org.example.navalbattle.controller.PositionController;
 
 import java.io.IOException;
 
+/**
+ * Represents the position stage (window) for the naval battle game.
+ * This class handles the loading of the FXML layout for the position view.
+ */
 public class PositionStage extends Stage {
     private PositionController positionController;
 
+    /**
+     * Constructs a PositionStage and initializes the position view by loading the FXML layout.
+     *
+     * @throws IOException if there is an error loading the FXML file
+     */
     public PositionStage() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/navalbattle/position-view.fxml"));
         Parent root;
@@ -32,21 +45,39 @@ public class PositionStage extends Stage {
         show();
     }
 
-    public PositionController getPositionController(){
+    /**
+     * Returns the position controller associated with this stage.
+     *
+     * @return the position controller
+     */
+    public PositionController getPositionController() {
         return positionController;
     }
 
+    /**
+     * Returns the singleton instance of PositionStage.
+     *
+     * @return the singleton instance of PositionStage
+     * @throws IOException if there is an error creating the instance
+     */
     public static PositionStage getInstance() throws IOException {
-        return  PlayerStageHolder.INSTANCE != null ?
+        return PlayerStageHolder.INSTANCE != null ?
                 PlayerStageHolder.INSTANCE :
                 (PlayerStageHolder.INSTANCE = new PositionStage());
     }
 
+    /**
+     * Deletes the singleton instance of PositionStage.
+     * Closes the stage and sets the instance to null.
+     */
     public static void deleteInstance() {
         PlayerStageHolder.INSTANCE.close();
         PlayerStageHolder.INSTANCE = null;
     }
 
+    /**
+     * Holder class for the singleton instance of PositionStage.
+     */
     private static class PlayerStageHolder {
         private static PositionStage INSTANCE;
     }
