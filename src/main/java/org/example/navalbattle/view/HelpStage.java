bf9895a8a4345b38b253fab4dc5ad52c9554d4d1
Jamh4949 <163215234+Jamh4949@users.noreply.github.com>
2024-06-07 14:58:36 -0500

    Update HelpStage.java

--- src/main/java/org/example/navalbattle/view/HelpStage.java
+++ src/main/java/org/example/navalbattle/view/HelpStage.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jose Martínez - Jhorman Gómez - Esteban Gómez
+ * @version final
+ */
 package org.example.navalbattle.view;
 
 import javafx.fxml.FXMLLoader;
@@ -5,12 +9,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import org.example.navalbattle.controller.GameController;
 
 import java.io.IOException;
 
+/**
+ * Represents the help stage (window) for the naval battle game.
+ * This class handles the loading of the FXML layout for the help view.
+ */
 public class HelpStage extends Stage {
 
+    /**
+     * Constructs a HelpStage and initializes the help view by loading the FXML layout.
+     *
+     * @throws IOException if there is an error loading the FXML file
+     */
     public HelpStage() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/navalbattle/help-view.fxml"));
         Parent root;
@@ -30,17 +42,30 @@ public class HelpStage extends Stage {
         show();
     }
 
+    /**
+     * Returns the singleton instance of HelpStage.
+     *
+     * @return the singleton instance of HelpStage
+     * @throws IOException if there is an error creating the instance
+     */
     public static HelpStage getInstance() throws IOException {
-        return  HelpStage.HelpStageHolder.INSTANCE != null ?
+        return HelpStage.HelpStageHolder.INSTANCE != null ?
                 HelpStage.HelpStageHolder.INSTANCE :
                 (HelpStage.HelpStageHolder.INSTANCE = new HelpStage());
     }
 
+    /**
+     * Deletes the singleton instance of HelpStage.
+     * Closes the stage and sets the instance to null.
+     */
     public static void deleteInstance() {
         HelpStage.HelpStageHolder.INSTANCE.close();
         HelpStage.HelpStageHolder.INSTANCE = null;
     }
 
+    /**
+     * Holder class for the singleton instance of HelpStage.
+     */
     private static class HelpStageHolder {
         private static HelpStage INSTANCE;
     }
